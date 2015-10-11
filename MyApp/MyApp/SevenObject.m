67a43ded9178773afa8bb33118468777857b9cf9
ljh <ljh@test.com>
2015-10-11 22:25:01 +0800

    seven

---
+++ MyApp/MyApp/SevenObject.m
@@ -0,0 +1,13 @@
+//
+//  SevenObject.m
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import "SevenObject.h"
+
+@implementation SevenObject
+
+@end
