417ef070c8e32af32c38203312867738a7bea264
ljh <ljh@test.com>
2015-10-11 21:46:45 +0800

    firstObject

---
+++ MyApp/MyApp/FirstObject.h
@@ -0,0 +1,13 @@
+//
+//  FirstObject.h
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstObject : NSObject
+
+@end
---
+++ MyApp/MyApp/FirstObject.m
@@ -0,0 +1,13 @@
+//
+//  FirstObject.m
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import "FirstObject.h"
+
+@implementation FirstObject
+
+@end
