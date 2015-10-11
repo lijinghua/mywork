bc245b6bb9797b349e7f6d51c1d95e5f21f75944
ljh <ljh@test.com>
2015-10-11 22:10:16 +0800

    four

---
+++ MyApp/MyApp/Fourobject.h
@@ -0,0 +1,13 @@
+//
+//  Fourobject.h
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fourobject : NSObject
+
+@end
---
+++ MyApp/MyApp/Fourobject.m
@@ -0,0 +1,14 @@
+
+//
+//  Fourobject.m
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import "Fourobject.h"
+
+@implementation Fourobject
+
+@end
