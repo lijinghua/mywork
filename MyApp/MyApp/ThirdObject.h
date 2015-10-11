9be1cd7a8f844d9a3803e308de6cd207c461c8a7
ljh <ljh@test.com>
2015-10-11 21:59:28 +0800

    third

---
+++ MyApp/MyApp/ThirdObject.h
@@ -0,0 +1,13 @@
+//
+//  ThirdObject.h
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdObject : NSObject
+
+@end
---
+++ MyApp/MyApp/ThirdObject.m
@@ -0,0 +1,13 @@
+//
+//  ThirdObject.m
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import "ThirdObject.h"
+
+@implementation ThirdObject
+
+@end
