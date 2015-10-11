e3b075489d542ac2e28088f54131558f185f7b7e
ljh <ljh@test.com>
2015-10-11 22:32:35 +0800

    Nine

---
+++ MyApp/MyApp/NineObject.h
@@ -0,0 +1,13 @@
+//
+//  NineObject.h
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NineObject : NSObject
+
+@end
