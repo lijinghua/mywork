ed0c32e09c9016e5f7d5f4e73cca15858a330ae9
ljh <ljh@test.com>
2015-10-11 22:33:42 +0800

    ten

---
+++ MyApp/MyApp/TenObject.h
@@ -0,0 +1,13 @@
+//
+//  TenObject.h
+//  MyApp
+//
+//  Created by lijinghua on 15/10/11.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TenObject : NSObject
+
+@end
